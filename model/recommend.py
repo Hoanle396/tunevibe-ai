f6eed90c55399216b6aef5602ca990e7dddd749b
Lê Hữu Hoàn <hoanle396@gmail.com>
2024-06-14 22:23:46 +0700

    refactor code

--- model/recommend.py
+++ model/recommend.py
@@ -7,20 +7,19 @@ from recommenders.models.surprise.surprise_utils import (
 )
 
 from app.db import mydb
-df = pd.read_sql_query(sql="SELECT * FROM `vote`", con=mydb)
-df= df[["userId","musicId","rate"]]
 
 
 class Recommend(object):
     def __init__(self):
         self.svd =surprise.SVD(random_state=0, n_factors=200, n_epochs=30, verbose=True)
-       
-        train_set = surprise.Dataset.load_from_df( df=df, reader=surprise.Reader("ml-100k")).build_full_trainset()
+        self.df = pd.read_sql_query(sql="SELECT * FROM `vote`", con=mydb)
+        self.df= self.df[["userId","musicId","rate"]]
+        train_set = surprise.Dataset.load_from_df( df=self.df, reader=surprise.Reader("ml-100k")).build_full_trainset()
         with Timer() as train_time:
           self.svd.fit(train_set)
 
     def predict(self):
-       predictions = predict(self.svd, df, usercol="userId", itemcol="musicId")
+       predictions = predict(self.svd, self.df, usercol="userId", itemcol="musicId")
        return predictions
 
 
