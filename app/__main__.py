972e18542527fcb8f5b3deff0d6ee44866c9ad9d
Lê Hữu Hoàn <hoanle396@gmail.com>
2024-06-19 23:37:09 +0700

    Add chatbot

--- app/__main__.py
+++ app/__main__.py
@@ -2,9 +2,11 @@ from app import app
 import app.settings as settings
 from controllers.route import registryRouter
 from model.recommend import Recommend
+from model.chatbot import ChatBot
 
 model = Recommend()
+chatbot = ChatBot()
 
 if __name__ == "__main__":
-    registryRouter(app, model)
+    registryRouter(app, model, chatbot)
     app.run(host=settings.BE_HOST, port=settings.BE_PORT, debug=settings.ENV != "PROD")
--- controllers/route.py
+++ controllers/route.py
@@ -1,3 +1,4 @@
+import datetime
 import os
 from flask import request
 from flask_reuploads import AUDIO, UploadSet, configure_uploads
@@ -7,35 +8,45 @@ from app import settings
 aai.settings.api_key = settings.API_KEY
 
 
-def registryRouter(app, model):
-    wavaudio = UploadSet('files', AUDIO)
+def registryRouter(app, model, chatbot):
+    wavaudio = UploadSet("files", AUDIO)
 
-    app.config['UPLOADED_FILES_DEST'] = './audio'
+    app.config["UPLOADED_FILES_DEST"] = "./audio"
     configure_uploads(app, wavaudio)
 
     @app.route("/api/predict", methods=["POST"])
     def Audio2Text():
         try:
             transcriber = aai.Transcriber()
-            filename = wavaudio.save(request.files['audio'])
-            audio = './audio/{}'.format(filename)
+            filename = wavaudio.save(request.files["audio"])
+            audio = "./audio/{}".format(filename)
             transcript = transcriber.transcribe(audio)
-            return {"status":"success","data":transcript.text},200
+            return {"status": "success", "data": transcript.text}, 200
         except:
-            return {"status":"success","data":"Could not detect lyrics"},400
+            return {"status": "success", "data": "Could not detect lyrics"}, 400
         finally:
             os.remove(audio)
 
     @app.route("/api/recommend", methods=["POST"])
     def recommend():
         try:
-            userId= request.json["userId"]
+            userId = request.json["userId"]
             transcript = model.predict()
-            transcript = transcript[(transcript["userId"]==int(userId))]
-            result = transcript[(transcript["prediction"]>3.5)].to_dict(orient="records")
-            return {"status":"success","data":result,"no":transcript.to_dict(orient="records")},200
+            transcript = transcript[(transcript["userId"] == int(userId))]
+            result = transcript[(transcript["prediction"] > 3.5)].to_dict(
+                orient="records"
+            )
+            return {
+                "status": "success",
+                "data": result,
+                "no": transcript.to_dict(orient="records"),
+            }, 200
         except Exception as e:
             print(e)
-            return {"status":"success","data":"Could not detect lyrics"},400
-        
-        
+            return {"status": "success", "data": "Could not detect lyrics"}, 400
+
+    @app.route("/api/chat", methods=["POST"])
+    def chat():
+        msg = request.json["msg"]
+        chat = str(chatbot.chat(msg))
+        return {"user": "BOT", "text": chat, "createdAt": datetime.datetime.now()}, 200
